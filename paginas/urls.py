bd2801b80fca73712e8d09bd6e37bcd394e28975
beasoaressz <beatriizsoares14@gmail.com>
2025-07-18 11:44:21 -0300

    criação de um app (modulo) e configs de views e urls

---
+++ paginas/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HomePageView
+
+# define a url padrao para a pagina inicial
+urlpatterns = [
+    # path:('endereco', MinhaView.asview(), name='nome_da_url')
+    path('inicio/', HomePageView.as_view(), name='inicio'),
+]
