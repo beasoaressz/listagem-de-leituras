024ae88ffa46de120a0434971f3bc35c2bd11027
beasoaressz <beatriizsoares14@gmail.com>
2025-07-18 11:06:20 -0300

    first commit

---
+++ paginas/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    # ao digitar a URL raiz, o Django irá renderizar o template index.html
+    template_name = 'index.html'
