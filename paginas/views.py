f4bb9656239a16c78a2a90885c8644250692a3b5
beasoaressz <beatriizsoares14@gmail.com>
2025-07-30 11:57:27 -0300

    style(main): estilo do conteudo principal do site adicionado

--- paginas/views.py
+++ paginas/views.py
@@ -1,10 +1,21 @@
 from django.views.generic import TemplateView
+# from django.shortcuts import render
 
 # Create your views here.
 
+# def meus_livros(request):
+#     livros_lendo = ["Dom Casmurro", "1984", "O Pequeno Príncipe"]
+#     return render(request, "paginas/index.html", {"livros_lendo": livros_lendo})
+
 class PaginaInicial(TemplateView):
-    # ao digitar a URL raiz, o Django irá renderizar o template index.html
     template_name = 'paginas/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['livros_lendo'] = ["Dom Casmurro", "1984"]
+        context['livros_quero_ler'] = ["A Revolução dos Bichos", "O Hobbit"]
+        context['livros_concluidos'] = ["Capitães da Areia", "Orgulho e Preconceito"]
+        return context
 
 class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'
